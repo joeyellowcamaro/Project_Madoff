f0bb38ad8c5c8a218d2f2f9521e1515c3e6d3906
Joey Green <josephleroygreen@gmail.com>
2017-10-14 21:43:04 -0500

    Add files via upload

---
+++ Project_Madoff/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YSQ.core.Quotes;
+
+namespace Project_Madoff
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new loginForm());
+
+
+            /* 
+            * 
+            var quote_service = new QuoteService();
+
+            var quotes = quote_service.Quote("MSFT", "GOOG").Return(QuoteReturnParameter.Symbol,
+                                                                    QuoteReturnParameter.Name,
+                                                                    QuoteReturnParameter.Open);
+
+
+            //Get info from the quotes
+            foreach (var quote in quotes)
+            {
+                Console.WriteLine("{0} - {1} - {2}", quote.Symbol, quote.Name, quote.Open);
+            }
+
+            */
+
+        }
+    }
+}
---
+++ Project_Madoff/loginForm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+
+namespace Project_Madoff
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void csvLocTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileBrowseBtn_Click(object sender, EventArgs e)
+        {
+
+            String name = this.nameTBox.Text;
+
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "CSV|*.csv", ValidateNames = true })
+            {
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    csvLocTBox.Text = ofd.SafeFileName;
+                    String csvLocation = ofd.FileName;
+
+                    //var textReader = File.Open(@"F:\College\CSCI 491\C_YSQ_Solution\test.csv");
+                    //var csv = new CsvReader(sr);
+                    //using (var reader = new StreamReader(file.InputStream))
+                    using (var sr = new StreamReader(csvLocation))
+                    {
+                        var csv = new CsvReader(sr);
+
+                        String profileName = csv.GetField(0);
+
+                        if (profileName.Equals(name))
+                        {
+                            MessageBox.Show("Error 404");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Success");
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            var userName = nameTBox.Text;
+        }
+
+        private void createProBtn_Click(object sender, EventArgs e)
+        {
+
+            createProfileForm create = new createProfileForm();
+            create.Show();
+
+        }
+    }
+}
